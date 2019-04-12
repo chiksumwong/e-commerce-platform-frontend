cc73b920dbfe74df0e4c1c5697ebe108fa5f5c20
CHIK SUM <chiksum.wong@gmail.com>
2019-04-12 21:17:41 +0800

    Navbar

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './router/index.js'
+import store from './store/index.js'
+
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
   router,
