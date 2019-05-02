fa0236cc2a5d5684ad7d5cebd46d6a4961095716
CHIK SUM <chiksum.wong@gmail.com>
2019-05-02 15:26:00 +0800

    Update Navbar.vue

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -115,7 +115,6 @@ import UserAPI from "@/api/User";
         })
         const res = await UserAPI.deleteUserById(this.$store.state.user.user_id);
         this.$store.dispatch('user/logout')
-        this.$router.push('/')
       }
     },
     computed: {
