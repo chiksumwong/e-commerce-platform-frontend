a8aa88f10336b0c104c540cb59d12dd10c58d136
CHIK SUM <chiksum.wong@gmail.com>
2020-03-09 11:35:29 +0800

    3 front size

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -3,7 +3,11 @@
         <footer class="bg-primary">
             <div class="container">
                 <b-row>
-                    <b-col><p class="m-0 text-left">A A A</p>
+                    <b-col><p class="m-0 text-left">
+                        <b-button @click="switchFrontSize('S')" variant="primary" style="font-size:12px;">A</b-button>
+                        <b-button @click="switchFrontSize('M')" variant="primary" style="font-size:16px;">A</b-button>
+                        <b-button @click="switchFrontSize('L')" variant="primary" style="font-size:20px;">A</b-button>
+                    </p>
                     </b-col>
                     <b-col><p class="m-0 text-center text-white">Copyright &copy; Wong Chik Sum 2019 </p></b-col>
                     <b-col>
@@ -20,6 +24,15 @@
 <script>
     export default {
         methods: {
+            switchFrontSize(size){
+                if (size == 'S'){
+                    document.querySelector('html').style.fontSize = '12px'
+                }else if (size == 'M'){
+                    document.querySelector('html').style.fontSize = '16px'
+                }else if (size == 'L'){
+                    document.querySelector('html').style.fontSize = '20px'
+                }
+            },
             switchLang(lang) {
                 this.$i18n.locale = lang
                 localStorage.setItem('locale', lang)
